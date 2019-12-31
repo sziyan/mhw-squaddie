16e949bef3114d259c6984c32536b4965720d47e
sziyan <sziyan@hotmai.com>
2020-01-01 01:57:02 +0800

    README.MD

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    API_KEY = 'AIzaSyDqeiZFoNLUkuLN7H3aMtG511cwd3Wkbr0'
+    client_id = 'kn_3nXzENsvUjQ'
+    client_secret = '1CLsrd4FIdoUEC0ga0tU4vuP-CM'
+    host = "mongodb+srv://ndg:P%40ssw0rd@ndg-3djuk.gcp.mongodb.net/sg?retryWrites=true&w=majority" #sg
+    #host = "mongodb+srv://ndg:P%40ssw0rd@ndg-3djuk.gcp.mongodb.net/test?retryWrites=true&w=majority" #test
+    token = '976932675:AAHRy9-sEEvbEfP8krrI2PkWESJmQADh888' #mhw
+    #token = '180665590:AAGEXQVVWTzpou9TBekb8oq59cjz2Fxp_gY' #ascension
